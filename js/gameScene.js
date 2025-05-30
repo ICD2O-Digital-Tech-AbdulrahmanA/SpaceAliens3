175ffc355fc6fbef8d299e5c856d3adf573b6009
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-05-30 15:11:17 +0000

    123

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,36 +7,36 @@
  * This class is the splash scene for the game
  */
 class GameScene extends Phaser.Scene {
-
+    
+    // create an alien
     createAlien() {
-        const alienXLocation = Math.floor(Math.random() * 1920) + 1
-        let alienXVelocity = Math.floor(Math.random() * 50) + 1
+        const alienXLocation = Math.floor(Math.random() * 1920) + 2
+        let alienXVelocity = Math.floor(Math.random() * 50) + 2
         alienXVelocity *= Math.round(Math.random()) ? 1 : -1
-        const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
+        const anAlien = this.physics.add.sprite(alienXLocation, -99, 'alien')
         anAlien.body.velocity.y = 200
         anAlien.body.velocity.x = alienXVelocity
         this.alienGroup.add(anAlien)
     }
+
     constructor() {
         super({ key: 'gameScene' });
 
         this.background = null
         this.ship = null
         this.fireMissile = false
+        this.isGameOver = false
         this.score = 0
         this.scoreText = null
         this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
         
         this.gameOverText = null
         this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', align: 'center' }
-        
-        this.isGameOver = false
-
     }
   
   
-    init (data) {
-    this.cameras.main.setBackgroundColor("AEA04B");
+    init(data) {
+        this.cameras.main.setBackgroundColor("AEA04B");
     }
   
     preload() {
@@ -46,50 +46,47 @@ class GameScene extends Phaser.Scene {
         this.load.image('ship', 'assets/spaceShip.png')
         this.load.image('missile', 'assets/missile.png')
         this.load.image('alien', 'assets/alien.png')
+        // sound
         this.load.audio('laser', 'assets/laser1.wav')
         this.load.audio('explosion', 'assets/barrelExploding.wav')
         this.load.audio('bomb', 'assets/bomb.wav')
-
-
     }
-  
+
     create(data) {
+        this.fireMissile = false
+        this.isGameOver = false
+        this.score = 0
         this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
         this.background.setOrigin(0, 0)
-
-        this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
-
+        this.scoreText = this.add.text(10, 10, 'score: ' + this.score.toString(), this.scoreTextStyle)
         this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
-
         this.missileGroup = this.physics.add.group()
-
         this.alienGroup = this.add.group()
         this.createAlien()
-
         this.physics.add.overlap(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
             alienCollide.destroy()
             missileCollide.destroy()
-            this.sound.play('explosion')
             this.score = this.score + 1
             this.scoreText.setText('Score: ' + this.score.toString())
+            this.sound.play('explosion')
             this.createAlien()
             this.createAlien()
         }.bind(this))
-
-        this.physics.add.overlap(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
-            this.isGameOver = tru
+    
+        this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
             this.sound.play('bomb')
             this.physics.pause()
             alienCollide.destroy()
             shipCollide.destroy()
-            this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over! Click to play again.', this.gameOverTextStyle).setOrigin(0.5)
+            this.isGameOver = true
+            this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
             this.gameOverText.setInteractive({ useHandCursor: true })
             this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
-            this.score = this.score - this.score
         }.bind(this))
-       }
-  
-    update(time, delta) { 
+    }
+    
+
+    update(time, delta) {
         if (this.isGameOver) {
             return
         }
